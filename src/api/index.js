a6b297f9fce00dea7cd45862b62ffaa2ccf0790e
Kerry Sabanty <kgsabanty@gmail.com>
2023-02-08 15:52:11 -0500

    initial commit

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const url = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(url, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        'X-RapidAPI-Key':
+          API_KEY,
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+      }
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
